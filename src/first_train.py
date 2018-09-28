44e7515191d30fe91a33494146c50ce2dcbb574b
Simon Schnake <schnakes@uh2ulnmpc22.desy.de>
2018-09-28 13:59:08 +0200

    added src and clean-up project-one from results

---
+++ src/first_train.py
@@ -0,0 +1,132 @@
+##############################################################
+#    ___           _                                         #
+#   / _ \__ _  ___| | ____ _  __ _  ___  ___                 #
+#  / /_)/ _` |/ __| |/ / _` |/ _` |/ _ \/ __|                #
+# / ___/ (_| | (__|   < (_| | (_| |  __/\__ \                #
+# \/    \__,_|\___|_|\_\__,_|\__, |\___||___/                #
+#                            |___/                           #
+##############################################################
+import numpy as np
+from keras.optimizers import Adam
+import h5py
+import pickle
+import tensorflow as tf
+
+from keras.layers import Input, Dense
+from keras.models import Model
+
+##############################################################
+#  _                 _ _                  ___      _         #
+# | | ___   __ _  __| (_)_ __   __ _     /   \__ _| |_ __ _  #
+# | |/ _ \ / _` |/ _` | | '_ \ / _` |   / /\ / _` | __/ _` | #
+# | | (_) | (_| | (_| | | | | | (_| |  / /_// (_| | || (_| | #
+# |_|\___/ \__,_|\__,_|_|_| |_|\__, | /___,' \__,_|\__\__,_| #
+#                              |___/                         #
+##############################################################
+
+data = h5py.File('../data/electron.h5', 'r')
+X_train = data['train']['X']
+Y_train = data['train']['Y']
+
+history = {'loss': [], 'val_loss': []}
+
+
+def firstModel():
+    # Define the input placeholder as a tensor with shape
+    # input_shape. Think of this as your input image
+    X_input = Input(shape=(1088,))
+    X = Dense(128, activation='relu', name='fc')(X_input)
+    X = Dense(128, activation='relu', name='fc1')(X)
+    X = Dense(128, activation='relu', name='fc2')(X)
+    X = Dense(10, activation='relu', name='fc3')(X)
+    X = Dense(1, activation='linear', name='exit')(X)
+    return Model(inputs=X_input, outputs=X, name='firstModel')
+
+
+# try:
+#     model = load_model('model.h5')
+# except IOError:
+#     print('model not found')
+#     exit()
+
+# try:
+#     history = pickle.load(open('history.p', 'rb'))
+# except IOError:
+#     print('no history file')
+#     history = {'loss': [], 'val_loss': []}
+
+#############################################################
+#  _____           _       _                   __     _     #
+# /__   \_ __ __ _(_)_ __ (_)_ __   __ _    /\ \ \___| |_   #
+#   / /\/ '__/ _` | | '_ \| | '_ \ / _` |  /  \/ / _ \ __|  #
+#  / /  | | | (_| | | | | | | | | | (_| | / /\  /  __/ |_   #
+#  \/   |_|  \__,_|_|_| |_|_|_| |_|\__, | \_\ \/ \___|\__|  #
+#                                  |___/                    #
+#############################################################
+
+model = firstModel()
+
+model.compile(optimizer='rmsprop',
+              loss='mse')
+epochs = 3
+batch_size = 128
+hist_update = model.fit(X_train,
+                        Y_train,
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        validation_split=0.2).history
+
+history.update([('loss',
+                 history['loss'] + hist_update['loss']),
+                ('val_loss',
+                 history['val_loss'] + hist_update['val_loss'])])
+
+
+def alt_loss(y_true, y_pred):
+    epsilon = tf.constant(np.float64(0.0000001))
+    lower_border = tf.constant(np.float64(0.))
+    upper_border = tf.constant(np.float64(10.))
+    two = tf.constant(np.float64(2.))
+    pi = tf.constant(np.float64(np.pi))
+    mu = y_pred
+    sigma = tf.sqrt(tf.abs(mu))
+    elements = tf.divide(tf.exp(tf.divide(- tf.square(mu - y_true),
+                                          two*tf.square(sigma))),
+                         tf.sqrt(two*pi)*sigma)
+    elements = tf.distributions.Normal(mu, sigma).prob(y_true)
+    a = tf.divide(mu-lower_border, tf.sqrt(two)*sigma+epsilon)
+    b = tf.divide(mu-upper_border, tf.sqrt(two)*sigma+epsilon)
+    norms = tf.abs(tf.erf(a) - tf.erf(b))
+    return -tf.reduce_mean(tf.log(tf.divide(elements,
+                                            norms + epsilon)
+                                  + epsilon))
+
+
+adamOpt = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-8)
+model.compile(optimizer='Adam', loss=alt_loss)
+
+epochs = 100
+batch_size = 256
+hist_update = model.fit(X_train,
+                        Y_train,
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        validation_split=0.2).history
+history.update([('loss',
+                 history['loss'] + hist_update['loss']),
+                ('val_loss',
+                 history['val_loss'] + hist_update['val_loss'])])
+
+
+##############################################################
+#  __             _                  ___      _              #
+# / _\ __ ___   _(_)_ __   __ _     /   \__ _| |_ __ _       #
+# \ \ / _` \ \ / / | '_ \ / _` |   / /\ / _` | __/ _` |      #
+# _\ \ (_| |\ V /| | | | | (_| |  / /_// (_| | || (_| |      #
+# \__/\__,_| \_/ |_|_| |_|\__, | /___,' \__,_|\__\__,_|      #
+#                         |___/                              #
+##############################################################
+
+pickle.dump(history, open("history.p", "wb"))
+
+model.save_weights('weights.h5')
