af7c2c871e9a0149720c6eb8eed977862d92fa34
Simon Schnake <schnakes@max-cmsg009.desy.de>
2018-09-29 04:31:46 +0200

    blub

--- src/likelihood-loss.py
+++ src/likelihood-loss.py
@@ -105,7 +105,7 @@ model.fit(X_train,
 opt = Adadelta(lr=0.01)
 model.compile(optimizer=opt, loss=likelihood_loss)
 
-epochs = 100
+epochs = 1000
 batch_size = 1024
 hist_update = model.fit(X_train,
                         Y_train,
