ec683e16838a7b2c26fe215525a153643ac1f21e
Simon Schnake <schnake.simon@gmail.com>
2018-10-01 13:19:58 +0200

    added adv-train.py

---
+++ src/adv-train.py
@@ -0,0 +1,148 @@
+# This code is a adaptation of the code
+# https://github.com/glouppe/paper-learning-to-pivot
+# compile every model after setting trainable flags again
+
+##############################################################
+#    ___           _                                         #
+#   / _ \__ _  ___| | ____ _  __ _  ___  ___                 #
+#  / /_)/ _` |/ __| |/ / _` |/ _` |/ _ \/ __|                #
+# / ___/ (_| | (__|   < (_| | (_| |  __/\__ \                #
+# \/    \__,_|\___|_|\_\__,_|\__, |\___||___/                #
+#                            |___/                           #
+##############################################################
+
+from keras import losses
+from keras.layers import Input, Dense, Lambda
+from keras.models import Model
+from keras.optimizers import Adagrad
+from keras.utils import np_utils
+from keras.callbacks import EarlyStopping
+
+import h5py
+import pickle
+import numpy as np
+
+##############################################################
+#  _                 _ _                  ___      _         #
+# | | ___   __ _  __| (_)_ __   __ _     /   \__ _| |_ __ _  #
+# | |/ _ \ / _` |/ _` | | '_ \ / _` |   / /\ / _` | __/ _` | #
+# | | (_) | (_| | (_| | | | | | (_| |  / /_// (_| | || (_| | #
+# |_|\___/ \__,_|\__,|__|_| |_|\__, | /___,' \__,_|\__\__,_| #
+#                              |___/                         #
+##############################################################
+
+data = h5py.File('../../data/electron.h5', 'r')
+X_train = data['train']['X']
+Y_train = data['train']['Y']
+
+history = {'loss': [], 'val_loss': []}
+
+##############################################################
+#                        _      _                            #
+#        /\/\   ___   __| | ___| |                           #
+#       /    \ / _ \ / _` |/ _ \ |                           #
+#      / /\/\ \ (_) | (_| |  __/ |                           #
+#      \/    \/\___/ \__,_|\___|_|                           #
+##############################################################
+
+lam = 0.5
+
+inputs = Input(shape=(X_train.shape[1],))
+Dx = Dense(128, activation="relu")(inputs)
+Dx = Dense(128, activation="relu")(Dx)
+Dx = Dense(128, activation="relu")(Dx)
+Dx = Dense(10, activation="relu")(Dx)
+Dx = Dense(1, activation="linear")(Dx)
+D = Model([inputs], [Dx])
+
+results = Input(shape=(Y_train.shape[1],))
+Rx = Lambda(lambda x: (x[0])/x[1]**0.5)([D(inputs), results])
+Rx = Dense(10, activation="relu")(Rx)
+Rx = Dense(20, activation="relu")(Rx)
+Rx = Dense(30, activation="relu")(Rx)
+Rx = Dense(40, activation="relu")(Rx)
+Rx = Dense(500, activation="softmax")(Rx)
+R = Model([inputs, results], [Rx])
+
+
+def make_loss_D(c):
+    def loss_D(y_true, y_pred):
+        return c * losses.mean_squared_error(y_pred, y_true)
+    return loss_D
+
+
+def make_loss_R(c):
+    def loss_R(z_true, z_pred):
+        return c * losses.categorical_crossentropy(z_pred, z_true)
+    return loss_R
+
+
+opt = Adagrad(lr=0.1)
+D.compile(loss=[make_loss_D(c=1.0)], optimizer=opt)
+
+DRf = Model([inputs, results], [D(inputs), R([inputs, results])])
+DRf.compile(loss=[make_loss_D(c=1.0), make_loss_R(c=-lam)], optimizer=opt)
+
+DfR = Model([inputs, results], [R([inputs, results])])
+DfR.compile(loss=[make_loss_R(c=1.0)], optimizer=opt)
+
+#############################################################
+#  _____           _       _                   __     _     #
+# /__   \_ __ __ _(_)_ __ (_)_ __   __ _    /\ \ \___| |_   #
+#   / /\/ '__/ _` | | '_ \| | '_ \ / _` |  /  \/ / _ \ __|  #
+#  / /  | | | (_| | | | | | | | | | (_| | / /\  /  __/ |_   #
+#  \/   |_|  \__,_|_|_| |_|_|_| |_|\__, | \_\ \/ \___|\__|  #
+#                                  |___/                    #
+#############################################################
+
+earlystop = EarlyStopping(monitor='val_loss',
+                          min_delta=0.01,
+                          patience=3,
+                          verbose=0, mode='auto')
+callbacks_list = [earlystop]
+
+batch_size = 128
+epochs = 20
+
+# D.load_weights("D_init_weights.h5")
+D.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size,
+      validation_split=0.1, callbacks=callbacks_list)
+
+bins = np.arange(0., 10., 10./500.)[:-1]
+Z_train = np.digitize(Y_train, bins=bins)
+Z_train = np_utils.to_categorical(Z_train, num_classes=500)
+
+for i in range(15):
+
+    # Fit R
+    D.trainable = False
+    R.trainable = True
+
+    DfR.compile(loss=[make_loss_R(c=1.0)],
+                optimizer=opt)
+    DfR.fit([X_train, Y_train],
+            Z_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_split=0.1,
+            callbacks=callbacks_list)
+
+    # Fit D
+    D.trainable = True
+    R.trainable = False
+    DRf.compile(loss=[make_loss_D(c=1.0),
+                      make_loss_R(c=-lam)],
+                optimizer=opt)
+    hist_update = DRf.fit([X_train, Y_train],
+                          [Y_train, Z_train],
+                          epochs=epochs,
+                          batch_size=batch_size,
+                          validation_split=0.1,
+                          callbacks=callbacks_list).history
+    history.update([('loss',
+                     history['loss'] + hist_update['loss']),
+                    ('val_loss',
+                     history['val_loss'] + hist_update['val_loss'])])
+
+    D.save_weights("adversarial_weights.h5")
+    pickle.dump(history, open("adverserial_history.p", "wb"))
