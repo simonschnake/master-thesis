0d08fabc630f62349c882dbd0f42e1a96ecdb6cf
Simon Schnake <schnakes@max-cmsg009.desy.de>
2018-10-01 09:26:24 +0200

    first part mse

--- src/likelihood-loss.py
+++ src/likelihood-loss.py
@@ -61,6 +61,7 @@ def likelihood_loss(y_true, y_pred):
     sigma = tf.sqrt(tf.abs(y_pred))
     first_part = tf.divide(tf.square(mu - y_true),
                            two*tf.square(sigma)+epsilon)
+    first_part = tf.square(mu - y_true)
     a = tf.divide(upper_border-mu, tf.sqrt(two)*sigma+epsilon)
     b = tf.divide(lower_border-mu, tf.sqrt(two)*sigma+epsilon)
     penalty = tf.abs(tf.erf(a) - tf.erf(b))
